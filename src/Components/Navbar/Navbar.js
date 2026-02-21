05499d9f3369bdb6336fee05ef97ef25e04d4d3d
danushreddy575-dev <danushreddy575@gmail.com>
2026-02-21 16:04:26 +0530

    mobile response3

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -42,18 +42,6 @@ function Navbar() {
       window.removeEventListener("authChanged", syncUser);
     };
   }, []);
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.innerWidth <= 768 && window.scrollY > 80) {
-        setShowMobileCategories(true);
-      } else {
-        setShowMobileCategories(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
 
 
   const handleLoginSuccess = () => {
